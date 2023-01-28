66bdf837d13ec3d01ec8f6aa0bb12a4985583985
mohammadehsan <mohammadehsan@10.67.228.176>
2023-01-28 11:52:33 -0500

    fghfhfhfhgf

---
+++ src/Assignments2/DisplayMonths.java
@@ -0,0 +1,41 @@
+package Assignments2;
+
+public class DisplayMonths {
+
+	public static void main(String[] args) {
+		
+		int month =10;
+		switch (month) {
+		
+		case 1:
+			System.out.println("January");
+			
+		break;
+			
+		case 2:
+			System.out.println("February");
+			
+		break;
+		
+		case 3:
+			System.out.println("March");
+			
+		break;
+		
+		case 4:
+			System.out.println("April");
+			
+		break;
+		
+		default:
+			
+			System.out.println("Summer");
+			
+		
+		
+			
+		}
+		
+	}
+
+}
