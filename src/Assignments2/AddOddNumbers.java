05f8bb02ff02a0736b458c3c9032bb1bbdcc176e
mohammadehsan <mohammadehsan@10.67.228.176>
2023-01-28 11:52:54 -0500

    y

---
+++ src/Assignments2/AddOddNumbers.java
@@ -0,0 +1,28 @@
+package Assignments2;
+
+public class AddOddNumbers {
+
+	public static void main(String[] args) {
+		
+		int add=0;
+		
+		for (int i=50; i<=70; i++) {
+			
+			if (i%2 !=0) {
+				
+				add = add + i;
+				
+			}
+			
+		}
+			
+			System.out.println(add);
+			
+			
+	}
+}
+		
+				
+				
+			
+	
