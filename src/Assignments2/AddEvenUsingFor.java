09d0501ccf02490c0f88c0b3f2c42488e7b7023a
mohammadehsan <mohammadehsan@mohammads-air>
2023-01-28 22:41:32 -0500

    added a class

---
+++ src/Assignments2/AddEvenUsingFor.java
@@ -0,0 +1,26 @@
+package Assignments2;
+
+public class AddEvenUsingFor {
+
+	public static void main(String[] args) {
+		 
+		int add =0;
+		for (int x=1; x<=20; x++) {
+		
+		
+			
+			if (x%2 ==0) {
+			
+			add =add + x;
+			}
+		}
+				
+			
+			System.out.println(add);
+		
+		
+		
+		
+	}
+
+}
